9e2981110ba4e2b4d58878ed07282b5dfae2fb50
Everton Soares Silva <everton_dev@hotmail.com>
2023-01-02 23:56:01 -0300

    Versão 1.0
    API do laravel funcionado, página de login criada.

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="page">
+    <LoginPage/>
+  </div>
+</template>
+
+<script setup>
+
+import LoginPage from '@/components/authentication/login/LoginPage.vue'
+
+/*  comunicação com minha API LARAVEL PHP Funcionando perfeitamente
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+
+
+  methods: {
+    async api_laravel() {
+
+       let request = await fetch('http://127.0.0.1:8000/api/auth/register',{
+        method: 'POST',
+
+        //JSON stringify é importante para transformar um objeto {} em uma string json
+        body: JSON.stringify({
+            name : "Maria almeida",
+            email : "maria@fdsafsadfsda.com",
+            password : "password"
+        }),
+        headers: {
+          "Content-type" : "application/json", 
+          'Accept' : 'application/json'
+        }
+       })
+
+       const data = await request.json()
+
+       console.log(data)
+    }
+  },
+
+  mounted()
+  {
+    this.api_laravel()
+  }
+} */
+</script>
+
+<style>
+
+</style>
---
+++ src/components/default/buttonPrimary.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="btn-primary">{{ btnInfo }}</button>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    defineProps( {
+        btnInfo : String
+    })
+
+</script>
+
+<style scoped>
+
+    .btn-primary {
+        @apply bg-gradient-to-r from-blue-500 to-blue-600 border-2 border-transparent text-white py-1 px-2 rounded-2xl w-full font-bold;
+        transition: .2s ease-in;
+    }
+
+    .btn-primary:hover {
+        @apply bg-gradient-to-r from-white to-white text-blue-600 border-2 border-blue-600
+    }
+
+</style>
+
